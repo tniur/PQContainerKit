65d5259f4a165e2ef3cf7d0cd397b2c4e635d278
Pavel Bobkov <pavel.bbkv1@gmail.com>
2026-01-29 02:52:56 +0300

    chore: add macOS(.v12)

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 let package = Package(
     name: "PQContainerKit",
     platforms: [
-        .iOS(.v26)
+        .iOS(.v26),
+        .macOS(.v12)
     ],
     products: [
         .library(name: "PQContainerKit", targets: ["PQContainerKit"])
