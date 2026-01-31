d895e84745d84d178392651cf777961e75028b04
Pavel Bobkov <pavel.bbkv1@gmail.com>
2026-01-31 18:13:40 +0300

    build: change swiftformat mode to lint only

--- Scripts/precommit.sh
+++ Scripts/precommit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-swift package plugin --allow-writing-to-package-directory swiftformat
+swift package plugin --allow-writing-to-package-directory swiftformat --lint
 
 swift test
