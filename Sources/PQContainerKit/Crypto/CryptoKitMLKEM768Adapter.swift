34eb40d5dff791a20c9b0fbab46c0a418b940e5e
Pavel Bobkov <pavel.bbkv1@gmail.com>
2026-02-01 16:55:47 +0300

    feat: add adapter for CryptoKit MLKEM768

---
+++ Sources/PQContainerKit/Crypto/CryptoKitMLKEM768Adapter.swift
@@ -0,0 +1,24 @@
+//
+//  CryptoKitMLKEM768Adapter.swift
+//  PQContainerKit
+//
+//  Created by Pavel Bobkov on 01.02.2026.
+//
+
+import CryptoKit
+import Foundation
+
+@available(iOS 26.0, macOS 26.0, *)
+enum CryptoKitMLKEM768Adapter {
+    static func generatePrivateKey() throws -> MLKEM768.PrivateKey {
+        try MLKEM768.PrivateKey()
+    }
+
+    static func publicKeyRaw(from privateKey: MLKEM768.PrivateKey) -> Data {
+        privateKey.publicKey.rawRepresentation
+    }
+
+    static func makePublicKey(fromRaw raw: Data) throws -> MLKEM768.PublicKey {
+        try MLKEM768.PublicKey(rawRepresentation: raw)
+    }
+}
